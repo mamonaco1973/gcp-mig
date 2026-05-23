9852a825620481da8767b659db67d4e4f8e7a114
Mike Monaco <mamonaco1973@gmail.com>
2026-05-22 20:07:03 -0400

    basic mig example

--- apply.sh
+++ apply.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 ./check_env.sh
 
 terraform -chdir=01-mig init
-terraform -chdir=01-mig apply -auto-approve
+terraform -chdir=01-mig apply -auto-approve || true
+# Nasty race condition - Simple work around
+terraform -chdir=01-mig apply -auto-approve 
 
 ./validate.sh
